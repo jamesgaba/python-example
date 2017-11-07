09b7e27e6e7676062942991f104d0aac5d50ef4d
jamesgaba <jamesgaba@gmail.com>
2017-11-06 21:12:42 -0800

    Prime finder addition

--- helloWorld.py
+++ helloWorld.py
@@ -46,8 +46,8 @@ elif age > 40:
 else:
     print("you're in between!")
 
-# prime finder
-for n in range(1950000, 2000000):
+# prime finder...
+for n in range(19500, 20000):
     for x in range(2, n):
         if n % x == 0:
             #print(n, 'can be factored as', x, 'x', n//x)
